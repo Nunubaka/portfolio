0984908387b96c16990f9cb45303c52778b22f92
Nunubaka <144690212+Nunubaka@users.noreply.github.com>
2023-10-02 02:44:09 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["Web Designer", "2D Designer","3D Designer",],
+    typeSpeed:20,
+        backSpeed:20,
+        loop:true,
+      });
